d2dec64e2cee7a8aa06127c963bf9219aee5186a
David Strauch <dmaster18@gmail.com>
2019-10-30 18:00:26 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,1 +1,7 @@
-class AddFavoriteFoodToArtists < 
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
+  def change
+    add_column :artists, :favorite_food, :string
+  end  
+    
+    
+end
